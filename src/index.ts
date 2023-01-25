75411e76a07f99972e1260ed6dab2cec40396dd4
Julio-Norberto <juliocsscl@gmail.com>
2023-01-25 14:04:52 -0300

    feat: create src and index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
+})
