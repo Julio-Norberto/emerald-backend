502b1d31c0f1f7c45a95b6ae670d9e83291cef10
Julio-Norberto <juliocsscl@gmail.com>
2023-01-27 13:13:27 -0300

    feat: create function of register and login users, and your routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express'
+import { loginUser, registerUser } from '../use-cases/userController'
 
 export const userRoutes = Router()
 
 userRoutes.get('/', (req, res) => {
   res.status(200).json({ message: "Sucesso!" })
 })
+
+userRoutes.post('/register', registerUser)
+userRoutes.post('/login', loginUser)
---
+++ src/use-cases/userController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express'
+import { User } from '../entities/user.js'
+import { createUserToken } from '../helpers/create-user-token.js'
+import { getToken } from '../helpers/get-token.js'
+
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+export const registerUser = async (req: Request, res: Response) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  if(!name || !email || !password || !confirmPassword) {
+    return res.status(422).json({ message: "Por favor preencha todos os campos!" })
+  }
+
+  if(password !== confirmPassword) {
+    return res.status(422).json({ message: "as senhas divergem" })
+  }
+
+  const userExists = await User.findOne({ email: email })
+
+  if(userExists) {
+    return res.status(422).json({ message: "Este e-mail já está cadastrado!" })
+  }
+
+  // Create password
+  const salt = await bcrypt.genSalt(12)
+  const hashPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = new User({
+    name,
+    email,
+    password: hashPassword
+  })
+
+  try {
+    await user.save()
+  } catch(err) {
+    console.log(err)
+    return res.status(500).json({ message: "Algo deu errado. Tente novamente mais tarde!" })
+  }
+
+  await createUserToken(user, req, res)
+}
+
+export const loginUser = async(req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  if(!email || !password) {
+    return res.status(422).json({ message: "Por favor preencha todos os campos!" })
+  }
+
+  const user = await User.findOne({ email: email })
+
+  if(!user) {
+    return res.status(404).json({ message: "Usuário não cadastrado" })
+  }
+
+  const checkIfPasswordMatch = await bcrypt.compare(password, user.password)
+
+  if(!checkIfPasswordMatch) {
+    return res.status(422).json({ message: "Senha ou login inválidos!" })
+  }
+
+  await createUserToken(user, req, res)
+}
