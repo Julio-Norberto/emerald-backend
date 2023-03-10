358e5a2a92d7560bf6d68b698c307a8440bc87fb
Julio-Norberto <juliocsscl@gmail.com>
2023-03-10 11:35:11 -0300

    feat: add default port

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { userRoutes } from './routes/userRoutes.js'
 import { expanseRoutes } from './routes/expanseRoutes.js'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
